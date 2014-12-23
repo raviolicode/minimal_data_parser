68f2e48f6df641f32cf35584d5f04c1b8eb75ce7
Lucia Escanellas <raviolicode@gmail.com>
2014-12-23 13:11:10 -0200

    Add summary listings data

--- data_parser.rb
+++ data_parser.rb
@@ -23,6 +23,7 @@ class DataParser
     @metadata.each do |metadata_item|
       add_data_from_criteria(metadata_item)
     end
+    add_summary_data
 
     File.write(output_filename, JSON.generate(@merged_data))
   end
@@ -35,10 +36,13 @@ class DataParser
     end
   end
 
+  # For a particular criteria
+  # I need to add data aggregated by provider
   def add_data_from_criteria(metadata_item)
     info = Scraper.scrape(metadata_item.filename, metadata_item.columns)
     append_stats(metadata_item, info)
 
+    # Each provider data id aggregated into the provider object
     info.each do |provider_data|
       provider_id = provider_data[:id]
       provider_data.delete(:id)
@@ -52,14 +56,27 @@ class DataParser
     end
   end
 
+  # Add stats for all providers
+  # @merged_data[:stats] will have the following structure:
+  # stats: {
+  #   tiempos_espera: {
+  #     medicina_general:
+  #       {10 => XX, 25, = XX, ..., 90 => XX, perc_with_data => YY}
+  #     ...
+  #   }
+  # }
+  #
+  # Where XX are the values of the percentiles for that criterium
+  # And YY is the percentage of providers that do have values
   def append_stats(metadata_item, info)
-    stats = @merged_data[:stats] #.find {|pdata| pdata[:stats] }[:stats]
+    stats = @merged_data[:stats]
     keys = metadata_item.columns.reject{ |key| key.to_s == 'id' }
 
     new_stats = keys.inject({}){ |result, key| result.merge(calculate_stats(info, key)) }
     stats.merge!({ metadata_item.attribute => new_stats })
   end
 
+  # Calculate percentiles of a particular criteria
   def calculate_stats(info, key)
     raw_data = info.map{ |i| i[key] }
     data = raw_data.compact
@@ -68,9 +85,93 @@ class DataParser
       map{ |perc| { perc => stats.percentile(perc) } }.
       inject({}){ |result, p| result.merge(p) }
 
-    percentiles.merge!( { :perc_with_data => (raw_data.count - data.count) * 1.0 / raw_data.count } )
+    percentiles.merge!( { perc_with_data: (raw_data.count - data.count) * 1.0 / raw_data.count } )
 
     { key => percentiles }
+  end
+
+  # TODO: refactor
+  # calculate data that will be displayed on the summary page
+  # this is mostly manual
+  # Assumes certain data is present
+  def add_summary_data
+      medicamentos = %w(
+        ticket_general_fonasa
+        ticket_general
+        topeados_fonasa
+        topeados
+      )
+
+      tickets = %w(
+        med_general
+        med_general_fonasa
+        pediatria_fonasa
+        pediatria
+        control_embarazo_fonasa
+        control_embarazo
+        control_ginecologia
+        otras_fonasa
+        otras
+        domicilio_fonasa
+        domicilio
+        odontologia_fonasa
+        odontologia
+        consulta_referencia_fonasa
+        consulta_referencia
+      )
+
+      tickets_urgentes = %w(
+        urgencia_fonasa
+        urgencia
+        urgencia_domicilio_fonasa
+        urgencia_domicilio
+      )
+
+      estudios = %w(
+        endoscopia_fonasa
+        endoscopia
+        ecografia_fonasa
+        ecografia
+        ecodoppler_fonasa
+        ecodoppler
+        rx_fonasa
+        rx
+        rx_torax_fonasa
+        rx_torax
+        rx_colon_fonasa
+        rx_colon
+        resonancia_fonasa
+        resonancia
+        tomografia_fonasa
+        tomografia
+        rutina_fonasa
+        rutina
+      )
+
+    @merged_data[:providers].each do |provider|
+      if (provider['precios'])
+        provider['precios'].merge!({
+          promedios: {
+            medicamentos: average(provider['precios'], medicamentos),
+            tickets: average(provider['precios'], tickets),
+            tickets_urgentes: average(provider['precios'], tickets_urgentes),
+            estudios: average(provider['precios'], estudios)
+          }
+        })
+      else
+        # I need to show empty data when I don't have the data
+        provider.merge!({ precios: { promedios: {} } })
+      end
+    end
+  end
+
+  # I want the average of the values of the selected keys
+  # given a hash "values",
+  # and an array of "selected_keys"
+  def average(values, selected_keys)
+    selected_values = selected_keys.map{|key| values[key.to_sym]}.compact
+    avg = selected_values.reduce(:+).to_f / selected_values.size unless selected_values.empty?
+    avg
   end
 end
 
